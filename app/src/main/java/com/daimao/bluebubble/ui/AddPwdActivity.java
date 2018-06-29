c6725d16cebfa6ea2c8acf0e500e75b15ed4edbf
bai-ge <13077790403@163.com>
2018-06-30 00:22:33 +0800

    优化添加记录界面

--- app/src/main/java/com/daimao/bluebubble/ui/AddPwdActivity.java
+++ app/src/main/java/com/daimao/bluebubble/ui/AddPwdActivity.java
@@ -4,10 +4,16 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -15,6 +21,7 @@ import android.widget.TextView;
 
 import com.daimao.bluebubble.BaseApplication;
 import com.daimao.bluebubble.R;
+import com.daimao.bluebubble.view.CircleImageView;
 
 import butterknife.BindView;
 import cn.droidlover.xdroidmvp.mvp.XActivity;
@@ -23,7 +30,7 @@ import cn.droidlover.xdroidmvp.mvp.XActivity;
  * Created by Ant on 2018/6/24.
  */
 
-public class AddPwdActivity extends XActivity {
+public class AddPwdActivity extends XActivity implements View.OnTouchListener, View.OnClickListener{
 
     private static final String[] groupNameStr = {"社交", "工作", "学习", "生活", "娱乐", "其它"};
     private ArrayAdapter<String> adapter;
@@ -33,19 +40,76 @@ public class AddPwdActivity extends XActivity {
     //    @BindView(R.id.spin_group)
     Spinner spinner;
 
-    @BindView(R.id.et_group)
-    EditText groupEditText;
-
     @BindView(R.id.btn_select_group)
-    TextView groupBtn;    // 分组的编辑按钮
+    TextView mBtnGroup;    // 分组的编辑按钮
 
+
+    /***************************/
     @BindView(R.id.btn_lock)
-    ImageButton lockImgBtn;
+    ImageButton mBtnLock;
+
+    @BindView(R.id.btn_share_qrcode)
+    ImageButton mBtnShareQrcode;
 
     @BindView(R.id.btn_share)
-    ImageButton shareImgBtn;
+    ImageButton mBtnShare;
 
-    private boolean isLock;
+    @BindView(R.id.btn_help)
+    ImageButton mBtnHelp;
+
+    @BindView(R.id.img_logo)
+    CircleImageView mImgLogo;
+
+    @BindView(R.id.ll_title)
+    ViewGroup mLayoutTitle;
+
+    @BindView(R.id.et_title)
+    EditText mEtTitle;
+
+    @BindView(R.id.ll_account)
+    ViewGroup mLayoutAccount;
+
+    @BindView(R.id.et_account)
+    EditText mEtAccount;
+
+    @BindView(R.id.ll_password)
+    ViewGroup mLayoutPassword;
+
+    @BindView(R.id.et_password)
+    EditText mEtPassword;
+
+    @BindView(R.id.checkbox_password)
+    CheckBox mCheckPassword;
+
+    @BindView(R.id.ll_address)
+    ViewGroup mLayoutAddress;
+
+    @BindView(R.id.et_address)
+    EditText mEtAddress;
+
+    @BindView(R.id.ll_group)
+    ViewGroup mLayoutGroup;
+
+    @BindView(R.id.et_group)
+    EditText mEtGroup;
+
+    @BindView(R.id.txt_group)
+    TextView mTxtGroup;
+
+    @BindView(R.id.ll_remark)
+    ViewGroup mLayoutRemark;
+
+    @BindView(R.id.et_remark)
+    EditText mEtRemark;
+
+    @BindView(R.id.btn_delete)
+    Button mBtnDelete;
+
+    @BindView(R.id.btn_save)
+    Button mBtnSave;
+
+
+    private boolean mIsLock;
 
     @Override
     public void initData(Bundle savedInstanceState) {
@@ -59,35 +123,58 @@ public class AddPwdActivity extends XActivity {
         // 添加事件Spinner事件监听
         // spinner.setOnItemSelectedListener(new SpinnerSelectedListener());
 
+        mLayoutTitle.setOnClickListener(this);
+        mLayoutAccount.setOnClickListener(this);
+        mLayoutPassword.setOnClickListener(this);
+        mLayoutAddress.setOnClickListener(this);
+        mLayoutGroup.setOnClickListener(this);
+        mLayoutRemark.setOnClickListener(this);
         // 分组 按钮 -> 选择已有项
-        groupBtn.setOnClickListener(new View.OnClickListener() {
+        mBtnGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 selectGroupDialog();
             }
         });
 
+        mEtRemark.setOnTouchListener(this);
+
 
         // 上锁、解锁
-        lockImgBtn.setOnClickListener(new View.OnClickListener() {
+        mBtnLock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isLock = !isLock;
-                if (isLock) {
-                    lockImgBtn.setBackgroundResource(R.drawable.ic_lock);
+                mIsLock = !mIsLock;
+                if (mIsLock) {
+                    mBtnLock.setBackgroundResource(R.drawable.ic_lock);
                     BaseApplication.getInstance().showTip("上锁");
                 } else {
-                    lockImgBtn.setBackgroundResource(R.drawable.ic_unlock);
+                    mBtnLock.setBackgroundResource(R.drawable.ic_unlock);
                     BaseApplication.getInstance().showTip("解锁");
                 }
             }
         });
 
         // 分享
-        shareImgBtn.setOnClickListener(new View.OnClickListener() {
+        mBtnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 BaseApplication.getInstance().showTip("分享");
+            }
+        });
+
+        mCheckPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isCheck) {
+                if(isCheck){
+                    //选择状态 显示明文--设置为可见的密码
+                    mEtPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    mEtPassword.setSelection(mEtPassword.getText().length());
+                }else{
+                    //默认状态显示密码--设置文本 要一起写才能起作用  InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD
+                    mEtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    mEtPassword.setSelection(mEtPassword.getText().length());
+                }
             }
         });
 
@@ -122,19 +209,22 @@ public class AddPwdActivity extends XActivity {
                     BaseApplication.getInstance().showTip(groupNameStr[item]);
                     // 关闭
                     groupDialog.dismiss();
-                    groupEditText.setText(groupNameStr[item]);
+                    mEtGroup.setText(groupNameStr[item]);
+                    mEtGroup.setSelection(mEtGroup.getText().length());
                 } else if (item == DialogInterface.BUTTON_POSITIVE) {
                     // 确定按钮
                     BaseApplication.getInstance().showTip("已选择：" + groupNameStr[item]);
                     // 关闭
                     groupDialog.dismiss();
-                    groupEditText.setText(groupNameStr[item]);
+                    mEtGroup.setText(groupNameStr[item]);
+                    mEtGroup.setSelection(mEtGroup.getText().length());
                 } else if (item == DialogInterface.BUTTON_NEGATIVE) {
                     // 取消按钮
                     BaseApplication.getInstance().showTip("没有选择");
                     // 关闭
                     groupDialog.dismiss();
-                    groupEditText.setText("请输入分组");
+                    mEtGroup.setText("请输入分组");
+                    mEtGroup.setSelection(mEtGroup.getText().length());
                 }
             }
         });
@@ -160,5 +250,70 @@ public class AddPwdActivity extends XActivity {
     @Override
     public Object newP() {
         return null;
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        //触摸的是EditText并且当前EditText可以滚动则将事件交给EditText处理；否则将事件交由其父类处理
+        if ((view.getId() == R.id.et_remark && canVerticalScroll(mEtRemark))) {
+            view.getParent().requestDisallowInterceptTouchEvent(true);
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                view.getParent().requestDisallowInterceptTouchEvent(false);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * EditText竖直方向是否可以滚动
+     * @param editText  需要判断的EditText
+     * @return  true：可以滚动   false：不可以滚动
+     */
+    private boolean canVerticalScroll(EditText editText) {
+        //滚动的距离
+        int scrollY = editText.getScrollY();
+        //控件内容的总高度
+        int scrollRange = editText.getLayout().getHeight();
+        //控件实际显示的高度
+        int scrollExtent = editText.getHeight() - editText.getCompoundPaddingTop() -editText.getCompoundPaddingBottom();
+        //控件内容总高度与实际显示高度的差值
+        int scrollDifference = scrollRange - scrollExtent;
+
+        if(scrollDifference == 0) {
+            return false;
+        }
+
+        return (scrollY > 0) || (scrollY < scrollDifference - 1);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ll_title:
+                mEtTitle.requestFocus();
+                mEtTitle.setSelection(mEtTitle.getText().length());
+                break;
+            case R.id.ll_account:
+                mEtAccount.requestFocus();
+                mEtAccount.setSelection(mEtAccount.getText().length());
+                break;
+            case R.id.ll_password:
+                mEtPassword.requestFocus();
+                mEtPassword.setSelection(mEtPassword.getText().length());
+                break;
+            case R.id.ll_address:
+                mEtAddress.requestFocus();
+                mEtAddress.setSelection(mEtAddress.getText().length());
+                break;
+            case R.id.ll_group:
+                mEtGroup.requestFocus();
+                mEtGroup.setSelection(mEtGroup.getText().length());
+                break;
+            case R.id.ll_remark:
+                mEtRemark.requestFocus();
+                mEtRemark.setSelection(mEtRemark.getText().length());
+                break;
+        }
     }
 }
